485d7eeb677d1ef14b105e3c74d43dda847eca0a
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-10-18 11:18:03 -0300

    este es el template finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from './Navbar'
 import Card from './Card'
+import Jumbotron from './Jumbotron'
+import Footer from './Footer'
 
 const App = () => {
   return (
       <>
       <Navbar />
+      <Jumbotron />
       <Card />
+      <Footer />
       </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+
+const Footer = () => {
+  return (
+    <div className="container-fluid bg-dark text-white d-flex justify-content-center p-3">Copyright@ my website</div>
+  )
+}
+
+export default Footer
